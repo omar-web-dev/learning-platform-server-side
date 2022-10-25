ec13d689f71c7a1b3e3085dba46d97fa6e1ee1eb
omarwebdeve <omarf977865@gmail.com>
2022-10-26 01:53:25 +0600

    dynamic course

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port =process.env.PORT || 5000
+
+
+const courses = require('./data/course.json')
+
 app.use(cors())
 
 
@@ -9,6 +13,18 @@ app.get('/', (req, res) => {
   res.send('Innovation-Hub is running') 
 })
 
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+ 
+app.get(`/course/:id`, (req, res) => {
+    const id = req.params.id;
+    const course = courses.find(cr => cr.id === id)
+    res.send(course)
+    console.log(id, 'id', hotel);
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
